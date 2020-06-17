5c6ec0225c82d31c5df25ca4a9333582170b9d6a
Anna Ostojska <anna.ostojskaa@gmail.com>
2020-06-17 15:37:37 +0200

    Show total number of products

--- lista_zakupow.py
+++ lista_zakupow.py
@@ -6,4 +6,5 @@ for item in lista_zakupow:
     produkty = ", ".join(lista_zakupow.get(item)).title()
     print(f"Idę do {str.capitalize(item)}, żeby kupić{produkty}")
     sum_of_products +=len(lista_zakupow.get(item))
-            
+
+print(f"W sklepie kupuję {sum_of_products} produktów")        
