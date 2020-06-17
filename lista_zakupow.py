1748967c563fb1b5e5ce406d3d41e729e9cd6de1
Anna Ostojska <anna.ostojskaa@gmail.com>
2020-06-17 15:36:10 +0200

    Show text with shopping list

--- lista_zakupow.py
+++ lista_zakupow.py
@@ -1,2 +1,9 @@
 lista_zakupow = {"piekarnia":["chleb","bulki","paczek"],\
                 "warzywniak":["marchew","seler","rukola"]}
+sum_of_products = 0
+
+for item in lista_zakupow:
+    produkty = ", ".join(lista_zakupow.get(item)).title()
+    print(f"Idę do {str.capitalize(item)}, żeby kupić{produkty}")
+    sum_of_products +=len(lista_zakupow.get(item))
+            
